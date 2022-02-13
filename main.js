3c377363acf55ac235fd49f8b9afc23b9d3059d0
nirajeetsaha <94179031+nirajeetsaha@users.noreply.github.com>
2022-02-13 13:15:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+video = "";
+status = "";
+object = "";
+
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    object = document.getElementById("object").value;
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+}
